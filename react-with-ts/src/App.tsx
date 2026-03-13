48b0c132e4e9e62c3bb360812bc99586da3b107d
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-03-13 22:40:11 +0300

    added ts usestate

--- react-with-ts/src/App.tsx
+++ react-with-ts/src/App.tsx
@@ -1,15 +1,26 @@
-// import { Welcome } from "./Welcome";
+import { useState } from "react";
 
-import { Greeting } from "./greeting";
-
-// import { UserInfo } from "./UserInfo";
-
-// import { ProductCard } from "./ProductCard";
-
+interface User {
+  name: string;
+  age: number;
+}
 function App() {
+  const [count, setCont] = useState<number>(0);
+  const [data, setData] = useState<User | null>(null);
+  const [tasks, setTasks] = useState<string[]>([])
   return (
     <>
-    <Greeting name="ayubdheere"  age={22}  />
+      <button onClick={() => setCont(count + 1)}>addition</button>
+      <p>{count}</p>
+      <button onClick={() => setData({ name: "ayubdheere", age: 23 })}>
+        bad me
+      </button>
+      <button onClick={() => setData(null)}>bad me</button>
+
+      <p>{data?.name}</p>
+
+      <button onClick={() => setTasks(["ayubabdirahman", " ", "ahmed", " ", "salah"])}>add TASKS</button>
+      <p>{tasks}</p>
     </>
   );
 }
