852615c4ef3031f252c0f1a13cab3d23b2212dea
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-03-14 13:31:18 +0300

    event handle

--- react-with-ts/src/App.tsx
+++ react-with-ts/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from "react";
-interface User {
-  name: string,
-  age: number
-}
-interface userData {
-  id: number
-  name: string,
-  username: string
-}
 function App() {
-  const [count, setCount] = useState<number>(0)
-  const [data, setData] = useState<User | null>(null)
-  const [task, setTask] = useState<userData[]>([])
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      console.log('change event', e.target.value)
+  }
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+      console.log('click event')
+  }
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+  }
   return <>
-  <button onClick={() => setCount(count + 1)}>Increment</button>
-  <button onClick={() => setCount(count - 1)}>Decrement</button>
-   <p>{count}</p>
-    <button onClick={() => setData({name: "ayubdheere", age: 22})}>bad me</button>
-   <button onClick={() => setData(null)}>click me</button>
-   <p>{data?.name}</p>
-
-   <button onClick={() => setTask([{id:1, name: "farah", username: "farah11"}])}>add Taks</button>
-   {
-    task.map(tasks => (
-      <div>
-        <p>{tasks.id}</p>
-        <p>{tasks.name}</p>
-        <p>{tasks.username}</p>
-      </div>
-    ))
-   }
+  <input type="text" onChange={handleChange} />
+  <button onClick={handleClick}>Click</button>
+  <form onSubmit={handleSubmit}>
+    <input type="text" />
+    <button type="submit">checked</button>
+  </form>
   </>;
 }
 
