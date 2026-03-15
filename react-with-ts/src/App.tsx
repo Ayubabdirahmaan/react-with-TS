ab0fcb1ba373576bad89d94bbbb3d231acfd99bc
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-03-15 14:28:56 +0300

    added costome hook in ts

--- react-with-ts/src/App.tsx
+++ react-with-ts/src/App.tsx
@@ -1,17 +1,36 @@
-import { ContactForm } from "./ContactForm";
+import { useEffect, useRef, useState } from "react";
+
+interface User {
+  name: string;
+  email: string;
+  phone: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<User | null>(null);
 
-  const handlesubmit = ({ username, email }: { username: string, email: string }) => {
-  if(username && email) {
-    
-  }
-  }
- return (
-   <>
-    <ContactForm onSubmit={handlesubmit} />
-  </>
- )
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(()=> {
+      if(inputRef.current) {
+        inputRef.current.focus()
+      }
+  },[])
+
+  useEffect(() => {
+    async function fetchUser() {
+      const response = await fetch("/api/user");
+      const data: User = await response.json();
+      setUsers(data);
+      console.log(data);
+    }
+    fetchUser();
+  }, []);
+
+  return <>
+  <input ref={inputRef} type="text" />
+  {<p>{users?.name}</p>}
+  </>;
 }
 
 export default App;
